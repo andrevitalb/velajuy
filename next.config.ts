677a4810c417e006806b148b2f631e75370c2b37
André Vital <andre@andrevital.com>
2026-05-14 00:11:55 -0500

    chore: promote typedRoutes from experimental in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next"
 
 const config: NextConfig = {
-	experimental: {
-		typedRoutes: true,
-	},
+	typedRoutes: true,
 	images: {
 		remotePatterns: [
 			{ protocol: "https", hostname: "*.r2.cloudflarestorage.com" },
